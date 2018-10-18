62462fdf51a90696ba70c6f02013a37c6c90cbbf
Lebranc Convas <33986491+lebrancconvas@users.noreply.github.com>
2018-10-18 14:23:10 +0700

    Petagoras Theory.

--- petagoras.js
+++ petagoras.js
@@ -4,4 +4,14 @@ let ort = (a,b) => {
         tri.push(i)
     }
     let trisquare = tri.map(x => x ** 2)
+    let crossorth = (a**2)+(b**2)
+    for (let j = 0 ; j < trisquare.length ; j++) {
+        if(crossorth == trisquare[j]) {
+            console.log(`triangle with length ${a} and ${b} is an orthagonal triangle`)
+        } 
+    }
 }
+
+ort(3,4)
+ort(5,12)
+ort(13,14)
